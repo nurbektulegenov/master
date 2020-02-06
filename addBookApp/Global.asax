a90369e628df7940da1038090d1cdb545d265faf
Нурбек Тулегенов <n_tulegenov@list.ru>
2020-02-06 15:28:42 +0600

    Add project files.

---
+++ addBookApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="addBookApp.MvcApplication" Language="C#" %>
---
+++ addBookApp/Models/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace addBookApp.Models
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("DbConnection") { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
